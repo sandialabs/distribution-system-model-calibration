ae28b33db1a044567a33b8cf40912535fda911bb
David Pinney <david@pinney.org>
2023-11-03 10:19:33 -0400

    rename package to avoid pip errors

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ from setuptools import setup
 
 with open('README.MD') as f:
 	long_description = f.read()
-	
+
 reqs = open('requirements.txt').readlines()
-	
+
 setup(
-	name='snl_dsmc',
+	name='sdsmc', #NOTE: dsmc was already taken as a pypi name
 	version='1.0.0',
-	description='SNL Distribution System Model Calibration Algorithms',
+	description='Sandia Distribution System Model Calibration Algorithms',
 	long_description_content_type='text/markdown',
 	long_description=long_description,
 	author='Logan Blakely',
 	author_email='lblakel@sandia.gov',
 	url='https://github.com/sandialabs/distribution-system-model-calibration',
-	packages = ['snl_dsmc'],
+	packages = ['sdsmc'],
 	include_package_data=True,
 	setup_requires=reqs,
 	install_requires=reqs
