14873a9e06953720cf2955dfb00cdeabc0172b9a
David Pinney <david@pinney.org>
2023-11-03 11:38:55 -0400

    Add find_packages() to setup.py

---
+++ sdsmc/MeterTransformerPairing/__init__.py
@@ -0,0 +1,1 @@
+print('MTP Init')
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.MD') as f:
 	long_description = f.read()
@@ -16,7 +16,7 @@ setup(
 	author='Logan Blakely',
 	author_email='lblakel@sandia.gov',
 	url='https://github.com/sandialabs/distribution-system-model-calibration',
-	packages = ['sdsmc'],
+	packages = find_packages(),
 	include_package_data=True,
 	setup_requires=reqs,
 	install_requires=reqs
