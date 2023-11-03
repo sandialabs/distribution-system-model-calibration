73d5423ffcd8aba550c057f8cf1846b45a79f0ce
David Pinney <david@pinney.org>
2023-11-03 11:21:10 -0400

    Refactor imports in MeterTransformerPairing package

--- sdsmc/__init__.py
+++ sdsmc/__init__.py
@@ -1,12 +1,11 @@
 if __package__ in [None, '']:
     import MeterTransformerPairing
 else:
-    from . import MeterTransformerPairing
+    from sdsmc import MeterTransformerPairing
 
 # from sdsmc import OnlinePhaseChangePoint
 # from sdsmc import PhaseIdentification
 #TODO: set up imports froms other sub-packages
-
 #TODO: import the specific functions from the submodules that you want to expose to the user
 
 def meter_transformer_pairing(in_csv,out_csv):
