da588e9b619ceaaf99fac04f68122b76dad1e8f9
David Pinney <david@pinney.org>
2023-11-03 10:07:12 -0400

    structured as valid python package

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.md') as f:
+	long_description = f.read()
+	
+reqs = open('requirements.txt').readlines()
+	
+setup(
+	name='snl_dsmc',
+	version='1.0.0',
+	description='SNL Distribution System Model Calibration Algorithms',
+	long_description_content_type='text/markdown',
+	long_description=long_description,
+	author='Logan Blakely',
+	author_email='lblakel@sandia.gov',
+	url='https://github.com/sandialabs/distribution-system-model-calibration',
+	packages = ['snl_dsmc'],
+	include_package_data=True,
+	setup_requires=reqs,
+	install_requires=reqs
+)
---
+++ snl_dsmc/__init__.py
@@ -0,0 +1,17 @@
+from MeterTransformerPairing import *
+from OnlinePhaseChangePoint import *
+from PhaseIdentification import *
+
+#TODO: don't star import the submodules, just import the functions needed to run the code.
+
+def meter_transformer_pairing(in_csv,out_csv):
+    pass #TODO: implement by calling the correct module function
+
+def online_phase_change_point(in_csv,out_csv):
+    pass #TODO: implement by calling the correct module function
+
+def phase_identification(in_csv,out_csv):
+    pass #TODO: implement by calling the correct module function
+
+def _run_all_tests():
+    pass #TODO: maybe add code to test each of the submodules here? I.e. just call some of the functions in the submodules on the existing sample data to make sure they don't crash.
