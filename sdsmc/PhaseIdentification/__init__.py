a6c25936ac435baa07dacbd48e23bbd03d0fcdd1
David Pinney <david@pinney.org>
2023-11-03 11:23:32 -0400

    Removed unnecessary imports in MeterTransformerPairing, OnlinePhaseChangePoint, and PhaseIdentification packages.

--- sdsmc/PhaseIdentification/__init__.py
+++
@@ -1,6 +0,0 @@
-# from sdsmc.PhaseIdentification import CA_Ensemble_Funcs.py
-# from sdsmc.PhaseIdentification import CA_Ensemble_SampleScripts.py
-# from sdsmc.PhaseIdentification import PhaseIdent_Utils.py
-# from sdsmc.PhaseIdentification import SensorMethod_Funcs.py
-# from sdsmc.PhaseIdentification import SensorMethod_SampleScripts.py
-#TODO: probably don't need all these imports.
--- sdsmc/__init__.py
+++ sdsmc/__init__.py
@@ -1,7 +1,7 @@
-if __package__ in [None, '']:
-    import MeterTransformerPairing
-else:
-    from sdsmc import MeterTransformerPairing
+# if __package__ in [None, '']:
+#     import MeterTransformerPairing
+# else:
+#     from sdsmc import MeterTransformerPairing
 
 # from sdsmc import OnlinePhaseChangePoint
 # from sdsmc import PhaseIdentification
