9c8f8216eb95088f3b1a7137e7353462ad9b7faf
David Pinney <david@pinney.org>
2023-11-03 11:16:53 -0400

    Commented out unnecessary imports in multiple files

--- sdsmc/MeterTransformerPairing/__init__.py
+++ sdsmc/MeterTransformerPairing/__init__.py
@@ -1,11 +1,11 @@
 if __package__ is None or __package__ == '':
     import M2TFuncs
     import M2TUtils
-    import MeterToTransPairingScripts
-    import MeterToTransPairingScript_WithDistance
+    # import MeterToTransPairingScripts
+    # import MeterToTransPairingScript_WithDistance
 else:
     from . import M2TFuncs
     from . import M2TUtils
-    from . import MeterToTransPairingScripts
-    from . import MeterToTransPairingScript_WithDistance
+    # from . import MeterToTransPairingScripts
+    # from . import MeterToTransPairingScript_WithDistance
 #TODO: probably don't need all these imports.
--- sdsmc/OnlinePhaseChangePoint/__init__.py
+++ sdsmc/OnlinePhaseChangePoint/__init__.py
@@ -1,5 +1,5 @@
-from sdsmc.OnlinePhaseChangePoint import ChangepointUtils
-from sdsmc.OnlinePhaseChangePoint import CreateTimeDurationCurve_Script
-from sdsmc.OnlinePhaseChangePoint import OnlineChangepoint_Script
-from sdsmc.OnlinePhaseChangePoint import OnlineChangepointFunctions
+# from sdsmc.OnlinePhaseChangePoint import ChangepointUtils
+# from sdsmc.OnlinePhaseChangePoint import CreateTimeDurationCurve_Script
+# from sdsmc.OnlinePhaseChangePoint import OnlineChangepoint_Script
+# from sdsmc.OnlinePhaseChangePoint import OnlineChangepointFunctions
 #TODO: probably don't need all these imports.
