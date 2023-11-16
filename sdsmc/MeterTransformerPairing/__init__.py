bdfda1a2376457f836d5f18e152a5c23858d9855
jenny <jenthakkar@gmail.com>
2023-11-16 02:38:54 -0500

    modified the init to pull the new funct not the script

--- sdsmc/MeterTransformerPairing/__init__.py
+++ sdsmc/MeterTransformerPairing/__init__.py
@@ -1,10 +1,8 @@
 if __package__ in [None, '']:
     import M2TFuncs
     import M2TUtils
-    import MeterToTransPairingScript_WithDistance
-    import MeterToTransPairingScripts
+    import TransformerPairing
 else:
     from . import M2TFuncs
     from . import M2TUtils
-    from . import MeterToTransPairingScript_WithDistance
-    from . import MeterToTransPairingScripts
+    from . import TransformerPairing
