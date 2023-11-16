68390191f4620391ff62edd44cdeaa2921d117b5
David Pinney <david@pinney.org>
2023-11-16 15:22:35 -0500

    Update __init__.py

--- sdsmc/__init__.py
+++ sdsmc/__init__.py
@@ -7,11 +7,5 @@ else:
     from . import OnlinePhaseChangePoint
     from . import PhaseIdentification
 
-def online_phase_change_point(in_csv,out_csv):
-    pass #TODO: implement by calling the correct submodule function
-
-def phase_identification(in_csv,out_csv):
-    pass #TODO: implement by calling the correct submodule function
-
 def _run_all_tests():
     pass #TODO: maybe add code to test each of the submodules here? I.e. just call some of the functions in the submodules on the existing sample data to make sure they don't crash.
