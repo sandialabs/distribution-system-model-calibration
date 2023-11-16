eeac5817a306d3ece28add4cdc943b473a09cfca
jenny <jenthakkar@gmail.com>
2023-11-16 14:38:20 -0500

    Removing unneeded inputs

--- sdsmc/MeterTransformerPairing/__init__.py
+++ sdsmc/MeterTransformerPairing/__init__.py
@@ -1,8 +1,4 @@
 if __package__ in [None, '']:
-    import M2TFuncs
-    import M2TUtils
     import TransformerPairing
 else:
-    from . import M2TFuncs
-    from . import M2TUtils
     from . import TransformerPairing
