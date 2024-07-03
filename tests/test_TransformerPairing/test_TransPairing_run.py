9d216818ed4f0dcfb4380e552dce31c39d7aa043
Jenny <jenthakkar@gmail.com>
2024-07-03 07:14:57 +0000

    Fixed .gitignore. Fixed readme to be correct with changes made in the past. Updated test to use unittest ( in python standard library ) instead of pytest

--- tests/test_TransformerPairing/test_TransPairing_run.py
+++ tests/test_TransformerPairing/test_TransPairing_run.py
@@ -1,5 +1,5 @@
 # Python Library Imports
-import pytest
+import unittest
 from pathlib import Path
 import os
 
@@ -9,23 +9,33 @@ from sdsmc.MeterTransformerPairing import TransformerPairing
 
 # Test Transformer Pairing funct.
 
-def test_transformerPairing_run():
-    useTrueLabels = True
+class TestingSDSMC( unittest.TestCase ):
 
-    # Relative pathing setup
-    currentDirectory = Path(__file__).parent.resolve()
-    sampleDataDirectory = Path( currentDirectory.parent.parent, 'sdsmc/SampleData')
-    saveResultsPath = currentDirectory
+    def test_transformerPairing_run( self ):
+        useTrueLabels = True
 
-    voltageInputPathCSV = Path( sampleDataDirectory, "voltageData_AMI.csv")
-    realPowerInputPathCSV = Path( sampleDataDirectory, "realPowerData_AMI.csv")
-    custIDInputPathCSV = Path( sampleDataDirectory, "CustomerIDs_AMI.csv")
-    reactivePowerInputPathCSV = Path( sampleDataDirectory, "reactivePowerData_AMI.csv")
-    transformerLabelsErrorsPathCSV = Path( sampleDataDirectory, 'TransformerLabelsErrors_AMI.csv')
+        # Relative pathing setup
+        currentDirectory = Path(__file__).parent.resolve()
+        sampleDataDirectory = Path( currentDirectory.parent.parent, 'sdsmc/SampleData')
+        saveResultsPath = currentDirectory
 
-    if useTrueLabels:
-        transformerLabelsTruePath = Path(sampleDataDirectory, 'TransformerLabelsTrue_AMI.csv' )
-    TransformerPairing.run( voltageInputPathCSV, realPowerInputPathCSV, reactivePowerInputPathCSV, custIDInputPathCSV, transformerLabelsErrorsPathCSV, transformerLabelsTruePath, saveResultsPath, useTrueLabels )
+        # Get sample data
+        voltageInputPathCSV = Path( sampleDataDirectory, "voltageData_AMI.csv")
+        realPowerInputPathCSV = Path( sampleDataDirectory, "realPowerData_AMI.csv")
+        custIDInputPathCSV = Path( sampleDataDirectory, "CustomerIDs_AMI.csv")
+        reactivePowerInputPathCSV = Path( sampleDataDirectory, "reactivePowerData_AMI.csv")
+        transformerLabelsErrorsPathCSV = Path( sampleDataDirectory, 'TransformerLabelsErrors_AMI.csv')
 
-    assert Path( currentDirectory, "outputs_ChangedCustomers_M2T.csv").exists() == True
-    assert Path( currentDirectory, "outputs_ImprovementStats.csv").exists() == True
+        if useTrueLabels:
+            transformerLabelsTruePath = Path(sampleDataDirectory, 'TransformerLabelsTrue_AMI.csv' )
+        TransformerPairing.run( voltageInputPathCSV, realPowerInputPathCSV, reactivePowerInputPathCSV, custIDInputPathCSV, transformerLabelsErrorsPathCSV, transformerLabelsTruePath, saveResultsPath, useTrueLabels )
+
+        # Test all necessary files are there and under the right name.
+
+        self.assertTrue( Path( currentDirectory, "outputs_ChangedCustomers_M2T.csv").exists() )
+        self.assertTrue( Path( currentDirectory, "outputs_ImprovementStats.csv").exists() )
+
+        # Test some values as a sanity check to ensure the calculations were correct.
+
+if __name__ == '__main__':
+    unittest.main()
